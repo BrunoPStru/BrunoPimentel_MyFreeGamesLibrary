54b450504b2f42fb48d57d56e825407cb73cf022
Bruno <brunopstruminski@gmail.com>
2022-08-25 13:32:25 -0300

    First Commit, project created

---
+++ src/main/java/br/inatel/myfreegameslibrary/BrunoPimentelMyFreeGamesLibraryApplication.java
@@ -0,0 +1,13 @@
+package br.inatel.myfreegameslibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrunoPimentelMyFreeGamesLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrunoPimentelMyFreeGamesLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/inatel/myfreegameslibrary/BrunoPimentelMyFreeGamesLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.inatel.myfreegameslibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrunoPimentelMyFreeGamesLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
